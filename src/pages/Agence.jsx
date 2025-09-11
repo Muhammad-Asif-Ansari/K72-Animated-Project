e25bfaa86117a81d0ac3faf5da958ef11a9ae601
Muhammad-Asif-Ansari <muhammadasifansari101@gmail.com>
2025-09-11 23:27:27 +0500

    paragraph added

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -4,8 +4,13 @@ const Agence = () => {
   return (
     <div className='font-[font2]'>
       <div>
-        <h1 className='text-[15vw] uppercase leading-[15vw]'>SEVEN7Y <br /> 
-        TW0</h1>
+        <h1 className='mt-[55vh] text-[15vw] uppercase leading-[15vw]'>SEVEN7Y <br />
+          TW0</h1>
+      </div>
+      <div className='pl-1/2'>
+        <p className='text-6xl w-1/2'>
+          We’re inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, but not influence that goes the distance. We bring that perspective to every brand story we help tell.
+        </p>
       </div>
     </div>
   )
